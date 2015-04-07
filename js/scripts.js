13887f841637544c79a0afccc909cc4b1e1893f8
Evan Butler & Connor Abdelnoor <student@epicodus.com>
2015-04-07 12:40:34 -0700

    added regexp

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,25 @@
 var titleCase = function(string) {
     var ignore = ["a", "an", "the", "and", "but", "or", "nor", "at", "by", "for", "from",
                   "in", "into", "of", "off", "on", "onto", "out", "over", "up", "with", "to", "as"];
-    var wordarrays = string.toLowerCase().split(" ");
-    var newarray = [];
-    var length = wordarrays.length -1;
-    wordarrays.forEach(function(wordarray) {
-        if ((ignore.indexOf(wordarray) !== -1) && (wordarray !== wordarrays[0]) && (wordarray !== wordarrays[length])) {
-            newarray.push(wordarray);
-        } else {
-            wordarray = wordarray.charAt(0).toUpperCase() + wordarray.slice(1);
-            newarray.push(wordarray);
-        }
-    });
-    return newarray.join(" ");
+    var str = string.toLowerCase();
+    function toUpper(match){
+        return match.toUpperCase();
+    };
+    var newStr = str.replace(/\b\w/g, toUpper);
+    console.log(newStr);
+    return newStr;
+    // var wordarrays = string.toLowerCase().split(" ");
+    // var newarray = [];
+    // var length = wordarrays.length -1;
+    // wordarrays.forEach(function(wordarray, index) {
+    //     if ((ignore.indexOf(wordarray) !== -1) && (index !== 0) && (index !== length)) {
+    //         newarray.push(wordarray);
+    //     } else {
+    //         wordarray = wordarray.charAt(0).toUpperCase() + wordarray.slice(1);
+    //         newarray.push(wordarray);
+    //     }
+    // });
+    // return newarray.join(" ");
 };
 
 
